b817ac84939ff15aa84d9f7bce02c42e097cd9b3
Dmytro Tarasenko <lordtda@Dmytros-MacBook-Pro.local>
2025-11-17 15:21:29 -0500

    added humidity to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,8 +2,12 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const queryParam = latitude + ',' + longitude;
-    // const queryParam = latitude + ',' + longitude;
-    const url = `https://api.weatherstack.com/current?access_key=532ee671c60cc779abcdc366389316af&query=${queryParam}&units=m`;
+
+    // there is only 100 request fora month => I use 2 accounts
+    // const access_key = '532ee671c60cc779abcdc366389316af';  //dm.a account
+    const access_key = '324a7b7871c2f97db829d86e1eea4628';  //lord account
+
+    const url = `https://api.weatherstack.com/current?access_key=${access_key}&query=${queryParam}&units=m`;
     console.log(url);
     request({url, json: true}, (err, { body }) => {
     // request({url, json: true}, (err, response) => {
@@ -15,7 +19,7 @@ const forecast = (latitude, longitude, callback) => {
             const weather = body.current;
             const weatherDescription = `${weather.weather_descriptions[0]}.
             It is currently ${weather.temperature} degrees out. It feels like ${weather.feelslike} degrees out.
-            There is ${weather.precip}% chance of rain.`;
+            The humidity is ${weather.humidity}%. There is ${weather.precip}% chance of rain.`;
             callback(undefined, weatherDescription);
         }
     });
